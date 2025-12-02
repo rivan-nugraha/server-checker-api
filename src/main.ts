e953b98ff97463a8fc4d482c9008105621740f4d
Irvan Nugraha <piksi.irvan.18402043@gmail.com>
2025-12-02 13:27:12 +0700

    Fixing

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   await app.register(fastifyMultipart);
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000, () => {
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0', () => {
     console.log(
       `Application Started at port: ${process.env.PORT ?? 3000}, httpsMode: ${httpsMode}`,
     );
